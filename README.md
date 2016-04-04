9b4893f9bd18fa3e952e8fcbf49528e278b70b31
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-04-04 21:14:43 +0200

    subCounter.py is perfected

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Can only roll one die at a time.
 The string "I like d8s, I really like d8s" will make it think you want to roll a d88.
 
 Eventually, I will want it to look at different TYPES of dice and return different results based on them, so it won't all hinge on "d". Part of this is because I want an IRC dicebot that can run Edge of Empire games, a Star Wars tabletop RPG with very strange dice that give you results of how many Successes and Advantages you have. 
+
+#subCounter.py
+
+This baby is useful for repairing .srt subtitle files.
+What it does is check the first number in front of each paragraph and make sure they're all one after the other.
+This will be very useful when I create my translation of the Force Awakens, where I will need to add slovenian
+translations of the crawl, which the english version does not have.
--- subCounter.py
+++ subCounter.py
@@ -4,34 +4,34 @@ import sys
 
 instructions = """subCounter.py takes a standard .srt subtitle file, which has a pattern like this:
 
-1
-00:02:56,510 --> 00:02:58,846
-This will begin to make things right.
+    1
+    00:02:56,510 --> 00:02:58,846
+    This will begin to make things right.
 
-2
-00:02:59,764 --> 00:03:02,750
-I've traveled too far and seen too much
+    2
+    00:02:59,764 --> 00:03:02,750
+    I've traveled too far and seen too much
 
-3
-00:03:02,849 --> 00:03:05,812
-to ignore the despair in the galaxy.
+    3
+    00:03:02,849 --> 00:03:05,812
+    to ignore the despair in the galaxy.
 
-It then counts how many 'paragraphs' there are in the file, and assigns each its correct number on top. You can either
-pass it one argument, the target file, in which case it will just print out, or give it two, the target file and the new
-file into which to pass its work."""
+    It then counts how many 'paragraphs' there are in the file, and assigns each its correct number on top. You can either
+    pass it one argument, the target file, in which case it will just print out, or give it two, the target file and the new
+    file into which to pass its work."""
 
 ##the usual enumerate
 def printnumerate(thing):
-	for index, element in enumerate(thing):
-		print index, element
+    for index, element in enumerate(thing):
+        print index, element
 
 ##a special enumerate that also prints the length of whatever it is enumerating, if possible
 def printnumerate2(thing):
-	for index, element in enumerate(thing):
-		try:
-			print "indx%i - (len%i) - %s" % (index, len(element), element)
-		except:
-			print index, element
+    for index, element in enumerate(thing):
+        try:
+            print "indx%i - (len%i) - %s" % (index, len(element), element)
+        except:
+            print index, element
 
 tried = 0
 failed = 0
@@ -39,44 +39,46 @@ total = 0
 
 
 if len(argv) == 1 or len(argv) > 3:
-	print instructions
-	sys.exit()
+    print instructions
+    sys.exit()
 
 elif len(argv) >= 2:
-	paragraphNo = 1
-	lines = []
-	mahFile = open(argv[1],"r")
-	print len(argv), argv
+    paragraphNo = 1
+    lines = []
+    mahFile = open(argv[1],"r")
+    print len(argv), argv
 
-	for line in mahFile:
-		lines.append(line)
+    for line in mahFile:
+        lines.append(line)
 
-	"""for x in range(30):
-		if len(lines[x]) == 2:
-			paragraphNo += 1
-			print "+++++++++++++++++++",
-		try:
-			print int(lines[x]), "NUMBAH"
-		except:
-			print paragraphNo,"||  ", len(lines[x]), lines[x]
-	"""
-	mahFile.close()
+    """for x in range(30):
+        if len(line) == 2:
+            paragraphNo += 1
+            print "+++++++++++++++++++",
+        try:
+            print int(line), "NUMBAH"
+        except:
+            print paragraphNo,"||  ", len(line), line
+    """
+    mahFile.close()
 
-	if len(argv) == 3:
-		nuFile = open(argv[2],"w")
-		for line in lines:
-			total += 1
-			try:
-				int(line)
-				nuFile.write(paragraphNo)
-				paragraphNo += 1
-				print int(line),paragraphNo
-				tried += 1
+    if len(argv) == 3:
+        nuFile = open(argv[2],"w")
+        for line in lines:
+            total += 1
+            
+            try:
+                nuuum = int(line[:len(line)-1:])
+                ##print nuuum, type(nuuum), "IT WOOOOOOOOOOOOOOOOOORKED\n"
+                print paragraphNo
+                nuFile.write(str(paragraphNo)+"\n")
+                paragraphNo += 1
+                tried += 1
+            except:
+                nuFile.write(line)
+                failed += 1
 
-			except:
-				nuFile.write(line)
-				failed += 1
-
+    nuFile.close()            
 print "TRIED = %i, FAILED = %i, TOTAL = %i=%i" % (tried, failed, tried+failed, total)
 
 ##TODO
