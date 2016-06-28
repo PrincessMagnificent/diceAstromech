45347301d61445a4f4d19453b99ffe514fb75974
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-06-28 11:25:18 +0200

    select a CSV from a list

--- CSV/GeoLiteCountryCSV/CSVreader2.py
+++ CSV/GeoLiteCountryCSV/CSVreader2.py
@@ -5,10 +5,28 @@ print "PROGRAM INITIATED"
 listOfDirs = os.listdir(os.getcwd())
 print len(listOfDirs), "things, they are", listOfDirs
 
+listOfCSV = []
+
 for filez in listOfDirs:
     if filez.endswith(".csv"):
-        print filez, "ENDS WITH .CSV YOOOOOOO"
-    else:
-        print "FGHAJSDJKASJKDKJASDJKKASDJKASDJAKSJDAKSJDKASJDAKSJD"
+        ##print filez, "ENDS WITH .CSV YOOOOOOO"
+        listOfCSV.append(filez)
+    ##else:
+    ##    print "FGHAJSDJK", filez, "RAAAAAAAAAAAASK"
 
-###How do you like this
+print "\nThere are %i CSV files in this directory" % (len(listOfCSV))
+
+for index, item in enumerate(listOfCSV):
+    print index+1, item
+
+while True:
+    try:
+        choice = raw_input("select: ")
+        choice = int(choice)
+    except:
+        print "You were meant to give me a number"
+        continue
+    else:
+        print "SELECTED ", listOfCSV[choice-1]
+        break
+        ##Of course at this point the program breaks if the number is too high
